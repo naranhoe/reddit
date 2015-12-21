3780babe7225a48e15dc066dacdf5fd1816fada8
Jorge Naranjo <j.naranjojr@gmail.com>
2015-12-21 16:24:44 -0500

    Generate comment scaffold & add simple_form gem

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'devise', '~> 3.5', '>= 3.5.2'
 gem 'better_errors'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'acts_as_votable', '~> 0.10.0'
+gem 'simple_form', '~> 3.2', '>= 3.2.1'
 
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :comments
   devise_for :users
   resources :links do
     member do
@@ -43,8 +45,7 @@ Rails.application.routes.draw do
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
-  #     resources :comments
-  #     resources :sales do
+  #     #     resources :sales do
   #       get 'recent', on: :collection
   #     end
   #   end
