d5de543cf859fae477a5bf631aa3c7bb26b6c12e
Jorge Naranjo <j.naranjojr@gmail.com>
2015-12-21 16:37:51 -0500

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :comments
+
   devise_for :users
   resources :links do
     member do
       put "like", to:    "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
 
   root to: "links#index"
